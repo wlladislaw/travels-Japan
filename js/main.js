8a31bf9f590cee37f1bec4991bb98ad0bce60862
Vladislav <wlladisla@gmail.com>
2022-08-31 21:56:44 +0300

    mobile menu

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
-(function(){
+(function () {
   const header = document.querySelector('.header');
   window.onscroll = () => {
-    if(window.scrollY > 120){
+    if (window.scrollY > 120) {
       header.classList.add('header_active');
     } else {
       header.classList.remove('header_active');
     }
-  }
-}());
+  };
+})();
+
+(function () {
+  const burgerItem = document.querySelector('.burger');
+  const menu = document.querySelector('.header_nav');
+  const closeItem = document.querySelector('.header_nav_close');
+  burgerItem.addEventListener('click', () => {
+    menu.classList.add('header_nav_active');
+  });
+  closeItem.addEventListener('click', () => {
+    menu.classList.remove('header_nav_active');
+  });
+})();
